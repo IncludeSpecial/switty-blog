45dd8f2a33412abcd484c586c1fb42408b44486a
Rustam Kutaev <115391516+IncludeSpecial@users.noreply.github.com>
2024-10-24 03:11:15 +0900

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,7 +31,7 @@ export default defineNuxtConfig({
     strict: true,
   },
   robots: {
-    mergeWithRobotsTxtPath: './robots.txt'
+    mergeWithRobotsTxtPath: './Robots.txt'
   },
   nitro: {
     prerender: {
