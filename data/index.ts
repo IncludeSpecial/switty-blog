284b98e929b41b1758f0e3fb9a58c3a35d515ec5
Rustam Kutaev <115391516+IncludeSpecial@users.noreply.github.com>
2024-11-11 07:44:17 +0900

    removed CommentSection

--- data/index.ts
+++ data/index.ts
@@ -1,86 +1,66 @@
+let fullName: string = "Рустам Кутаев";
+
 export const navbarData = {
-  homeTitle: 'Switty\'s Blog | Блог',
-}
+  homeTitle: "Switty's Blog | Блог Свитти",
+};
 
 export const footerData = {
-  author: 'Rustam Kutaev',
-  aboutAuthor: 'Привет! Я Рустам , энтузиаст технологий, инженер-программист. В настоящее время Web-Developer',
-  authorInterest: 'Если у вас есть интересная идея, как с открытым исходным кодом, так и платная, давайте работать вместе.',
-  aboutTheSite: 'Это персональный блог, созданный с помощью Nuxt3, TailwindCSS, NuxtContent, Nuxt Icon.',
-}
-
-export const homePage = {
-  title: 'Добро пожаловать на мой сайт',
-  description: 'Получите информацию по веб-разработке, соответствующие статьи, советы, обучающие ресурсы и многое другое.',
-}
-
-export const blogsPage = {
-  title: 'Все посты',
-  description: 'Здесь вы найдете все записи, которые я написал и опубликовал на этом сайте.',
-}
-
-export const categoryPage = {
-  title: 'Категории',
-  description: 'Эта категория создается на основе всех тегов, упомянутых в различных записях блога.',
-}
+  author: fullName,
+  aboutAuthor:
+    "Привет! Я Рустам и в настоящее время я Frontend Web-Developer",
+  authorInterest:
+    "Если у вас есть интересная идея, как с открытым исходным кодом, так и платная, давайте работать вместе.",
+  aboutTheSite:
+    "Это персональный блог, созданный с помощью фреймворков Nuxt3  и TailwindCSS",
+};
 
 export const aboutPage = {
-  title: 'Rustam Kutaev',
-  description: 'Инженер-программист, специалист по решению проблем, веб-энтузиаст.',
-  aboutMe: 'Привет, Мир! Я разработчик программного обеспечения, который большую часть своего дня проводит за написанием кода в JetBrains в команде со своими друзьями. Когда я не занимаюсь написанием кода, вы можете увидеть, как я предлагаю решения проблем онлайн!',
-}
+  title: fullName,
+  description: "Инженер-программист, специалист по решению проблем и их созданию, web-developer.",
+  aboutMe:
+    "Я разработчик программного обеспечения, который большую часть своего дня проводит за написанием кода  в команде со своими друзьями или в одиночку. Когда я не занимаюсь написанием кода, делаю макеты в Figma!",
+};
+
+export const homePage = {
+  title: "Главная",
+  description:
+    "Получите информацию по веб-разработке, соответствующие статьи, советы, обучающие ресурсы и многое другое.",
+};
+
+export const blogsPage = {
+  title: "Блог",
+  description: "Здесь вы найдете все записи, которые я написал и опубликовал на этом сайте.",
+};
+
+export const categoryPage = {
+  title: "Категории",
+  description:
+    "Эта категория создается на основе всех тегов, упомянутых в различных записях блога.",
+};
 
 export const seoData = {
-  description: 'Switty, инженер-программист, с опытом работы в области разработки программного обеспечения более 2 лет.',
-  ogTitle: 'Увлечен кодингом и помогаю другим изучать Javascript, Typescript, Vue, Nuxt и решать проблемы.',
-  twitterDescription: 'Сайт моего блога, где я играю с Nuxt, Vue и т.д., а также демонстрирую свой блог, ресурсы и т.д.',
-  image: 'https://res.cloudinary.com/dmecmyphj/image/upload/v1673548905/nuxt-blog/cover_ntgs6u.webp',
-  mySite: 'https://switty.ru/',
-  twitterHandle: '@Switty',
-  mailAddress: 'IncludeSpecial@gmail.com',
-}
+  description: "Switty, инженер-программист, Web-developer с опытом работы в области разработки программного обеспечения более 2 лет.",
+  ogTitle: "Занимаюсь кодом и помогаю другим изучать Nuxt3, Tailwindcss, решать проблемы и просто пишу различные статьи.",
+  twitterDescription:
+    "Сайт моего блога, где я программирую на Nuxt, Vue и т.д., а также демонстрирую свой блог, ресурсы и т.д.",
+  image: "https://res.cloudinary.com/dmecmyphj/image/upload/v1673548905/nuxt-blog/cover_ntgs6u.webp",
+  mySite: "https://switty.ru/",
+  twitterHandle: "@Switty",
+  mailAddress: "IncludeSpecial@gmail.com",
+};
 
 export const siteMetaData = [
-  {
-    name: 'description',
-    content: seoData.description,
-  },
-  // Test on: https://developers.facebook.com/tools/debug/ or https://socialsharepreview.com/
-  { property: 'og:site_name', content: seoData.mySite },
-  { property: 'og:type', content: 'website' },
-  {
-    property: 'og:url',
-    content: seoData.mySite,
-  },
-  {
-    property: 'og:title',
-    content: seoData.ogTitle,
-  },
-  {
-    property: 'og:description',
-    content: seoData.description,
-  },
-  {
-    property: 'og:image',
-    content: seoData.image,
-  },
-  // Test on: https://cards-dev.twitter.com/validator or https://socialsharepreview.com/
-  { name: 'twitter:site', content: seoData.twitterHandle },
-  { name: 'twitter:card', content: 'summary_large_image' },
-  {
-    name: 'twitter:url',
-    content: seoData.mySite,
-  },
-  {
-    name: 'twitter:title',
-    content: seoData.ogTitle,
-  },
-  {
-    name: 'twitter:description',
-    content: seoData.twitterDescription,
-  },
-  {
-    name: 'twitter:image',
-    content: seoData.image,
-  },
-]
+  { name: "description", content: seoData.description },
+  { property: "og:site_name", content: seoData.mySite },
+  { property: "og:type", content: "website" },
+  { property: "og:url", content: seoData.mySite },
+  { property: "og:title", content: seoData.ogTitle },
+  { property: "og:description", content: seoData.description },
+  { property: "og:image", content: seoData.image },
+  { name: "twitter:site", content: seoData.twitterHandle },
+  { name: "twitter:card", content: "summary_large_image" },
+  { name: "twitter:url", content: seoData.mySite },
+  { name: "twitter:title", content: seoData.ogTitle },
+  { name: "twitter:description", content: seoData.twitterDescription },
+  { name: "twitter:image", content: seoData.image },
+];
--- pages/administrator.vue
+++ pages/administrator.vue
@@ -29,29 +29,30 @@ onMounted(loadUserStatus)
   <div class="py-5">
     <div class="container max-w-5xl mx-auto px-6 sm:py-9">
       <h1 class="text-3xl font-bold pb-5">Режим администратора</h1>
+      <p> Увы но тут пусто))</p>
       <div class="pb-5">
         <p v-if="isAdmin" class="text-lg text-green-600">Вы вошли как администратор.</p>
         <p v-else class="text-lg text-red-600">Вы не являетесь администратором.</p>
       </div>
-      <div class="space-x-4">
-        <button
-          v-if="!isAdmin"
-          @click="enableAdminMode"
-          class="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700"
-        >
-          Включить режим администратора
-        </button>
-        <button
-          v-else
-          @click="disableAdminMode"
-          class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"
-        >
-          Выключить режим администратора
-        </button>
-      </div>
+<!--      <div class="space-x-4">-->
+<!--        <button-->
+<!--          v-if="!isAdmin"-->
+<!--          @click="enableAdminMode"-->
+<!--          class="px-4 py-2 bg-green-600 text-white rounded-md hover:bg-green-700"-->
+<!--        >-->
+<!--          Включить режим администратора-->
+<!--        </button>-->
+<!--        <button-->
+<!--          v-else-->
+<!--          @click="disableAdminMode"-->
+<!--          class="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700"-->
+<!--        >-->
+<!--          Выключить режим администратора-->
+<!--        </button>-->
+<!--      </div>-->
 
       <!-- Передаем параметры isAdmin и currentUserId в CommentSection -->
-      <CommentSection :postId="'examplePostId'" :isAdmin="isAdmin" :userId="currentUserId" />
+<!--      <CommentSection :postId="'examplePostId'" :isAdmin="isAdmin" :userId="currentUserId" />-->
     </div>
   </div>
 </template>
